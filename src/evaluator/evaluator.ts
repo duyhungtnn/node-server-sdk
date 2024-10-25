8333b1b1b3172ecabc356cede9e9320556514e32
duyhungtnn <duyhung71089@gmail.com>
2024-10-25 22:26:41 +0700

    feat: add LocalEvaluator

--- src/evaluator/evaluator.ts
+++ src/evaluator/evaluator.ts
@@ -1,15 +1,21 @@
-import { createUser, Evaluator, Feature, SegmentUser, SegmentUsers } from '@bucketeer/node-evaluation';
+import {
+  createUser,
+  Evaluator,
+  Feature,
+  SegmentUser,
+  SegmentUsers,
+} from '@bucketeer/node-evaluation';
 import { FeaturesCache } from '../cache/features';
 import { SegmentUsersCache } from '../cache/segementUsers';
 import { Evaluation } from '../objects/evaluation';
 import { User } from '../objects/user';
 import { UserEvaluations } from '@bucketeer/node-evaluation/lib/proto/feature/evaluation_pb';
 
-interface EvaluateLocally {
+interface NodeEvaluator {
   evaluate(user: User, featureID: string): Promise<Evaluation>;
 }
 
-class NodeEvaluator implements EvaluateLocally {
+class LocalEvaluator implements NodeEvaluator {
   private tag: string;
   private featureCache: FeaturesCache;
   private segementUsersCache: SegmentUsersCache;
@@ -38,29 +44,34 @@ class NodeEvaluator implements EvaluateLocally {
     });
 
     const protoUser = createUser(user.id, user.data);
-    const userEvaluations = await evaluator.evaluateFeatures(targetFeatures, protoUser, segmentUsersMap, this.tag);
+    const userEvaluations = await evaluator.evaluateFeatures(
+      targetFeatures,
+      protoUser,
+      segmentUsersMap,
+      this.tag,
+    );
 
     const evaluation = this.findEvaluation(userEvaluations, featureID);
     return evaluation;
   }
 
   findEvaluation(userEvaluations: UserEvaluations, featureId: String): Evaluation {
-      for (const evaluation of userEvaluations.getEvaluationsList()) {
-          if (evaluation.getFeatureId() === featureId) {
-              return {
-                  id: evaluation.getId(),
-                  featureId: evaluation.getFeatureId(),
-                  featureVersion: evaluation.getFeatureVersion(),
-                  userId: evaluation.getUserId(),
-                  variationId: evaluation.getVariationId(),
-                  variationName: evaluation.getVariationName(),
-                  variationValue: evaluation.getVariationValue(),
-              };
-          }
+    for (const evaluation of userEvaluations.getEvaluationsList()) {
+      if (evaluation.getFeatureId() === featureId) {
+        return {
+          id: evaluation.getId(),
+          featureId: evaluation.getFeatureId(),
+          featureVersion: evaluation.getFeatureVersion(),
+          userId: evaluation.getUserId(),
+          variationId: evaluation.getVariationId(),
+          variationName: evaluation.getVariationName(),
+          variationValue: evaluation.getVariationValue(),
+        };
       }
+    }
 
-      throw new Error('Evaluation not found');
-  };
+    throw new Error('Evaluation not found');
+  }
 
   async getTargetFeatures(feature: Feature): Promise<Feature[]> {
     const targetFeatures: Feature[] = [feature];
@@ -92,4 +103,4 @@ class NodeEvaluator implements EvaluateLocally {
   }
 }
 
-export { EvaluateLocally };
+export { LocalEvaluator, NodeEvaluator };
